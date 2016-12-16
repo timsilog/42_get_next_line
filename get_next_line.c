637bb8f5e7027aee171f8b75452cf22d47cdf6bb
Tim Jose <timjose3@gmail.com>
2016-12-15 19:48:31 -0800

    just started

---
+++ get_next_line.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjose <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/15 19:43:01 by tjose             #+#    #+#             */
+/*   Updated: 2016/12/15 19:47:27 by tjose            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		get_next_line(const int fd, char **line)
+{
+	char	buff[BUFF_SIZE];
+	int		ret;
+
+	while ()//i dunno yet
+	{
+		ret = read(fd, buff, BUFF_SIZE);
+
+	}
+}
